7e302f56ad65d32837a5606f6ecf2340d04a9c7d
Kakami Souma <nkc.souma.kakami@gmail.com>
2018-09-03 13:47:59 +0900

    EnemyControlで敵が動くように修正

--- Assets/kakami/EnemyControl.cs
+++ Assets/kakami/EnemyControl.cs
@@ -4,41 +4,50 @@ using UnityEngine;
 
 public class EnemyControl : MonoBehaviour {
 
-    [SerializeField, Header("敵の動き(左右)")]
+    [SerializeField, Header("敵の動きtrueが右")]
     bool moveChangeLeftRight;        //敵の左右の動きを変える
-
-    [SerializeField, Header("敵の行動")]
+    [SerializeField, Header("敵の行動trueが行ったり来たり")]
     bool modeChange;                //敵の動きを変える
 
     [SerializeField,Header("速度")]
     float speed;                    //敵の速度
 
+    [SerializeField, Header("距離")]
+    float distance=4;               //距離
 
+    Vector2 fastPos;                //始まった時の位置
 
     void EnemyMove()
     {
-        switch (moveChangeLeftRight)
-        {
-            case true:
-                transform.position += new Vector3(speed,0,0);
-                break;
-
-            case false:
-                transform.position += new Vector3(speed*-1, 0, 0);
-                //if (transform.position.x)
-                //{
-
-                //}
-                break;
-        }
-
         switch (modeChange)
         {
             case true:
+                switch (moveChangeLeftRight)
+                {
+                    case true:
+                        transform.position += new Vector3(speed, 0, 0);
+                        if (transform.position.x >= distance || transform.position.x <= -distance)
+                        {
+                            moveChangeLeftRight = false;
+                        }
+                        break;
 
+                    case false:
+                        transform.position += new Vector3(speed * -1, 0, 0);
+                        if (transform.position.x >= distance || transform.position.x <= -distance)
+                        {
+                            moveChangeLeftRight = true;
+                        }
+                        break;
+                }
                 break;
 
             case false:
+                transform.position += new Vector3(speed, 0, 0);
+                if (transform.position.x >= distance || transform.position.x <= -distance)
+                {
+                    transform.position = fastPos;
+                }
 
                 break;
         }
@@ -46,12 +55,12 @@ public class EnemyControl : MonoBehaviour {
 
 	void Start ()
     {
-		
+        fastPos = transform.position;
 	}
 	
 
 	void Update ()
     {
-		
+        EnemyMove();
 	}
 }
