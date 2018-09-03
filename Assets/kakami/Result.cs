800546b6b4350aaaf6c3bf860efa61918e078f7d
Kakami Souma <nkc.souma.kakami@gmail.com>
2018-09-03 14:17:48 +0900

    result完成

---
+++ Assets/kakami/Result.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+    GameObject IntermediateObj;
+
+    float score;            //プレイヤーが取得したスコア
+    [SerializeField,Header("乗算させる値")]
+    float multiplication;   //スコアに乗算する数値
+
+    Text resultText;        //resultを表示させるText
+    // Use this for initialization
+    void Start ()
+    {
+        resultText = GameObject.Find("ResultText").GetComponent<Text>();
+        IntermediateObj = GameObject.Find("IntermediateObj");
+        IntermediateObj.SetActive(false);
+        GameMaster.Instance.OnGameOver+= ResultTextWriting;
+    }
+    void ResultTextWriting()
+    {
+        score = GameMaster.Instance.PlayerHeight;
+        score = score * multiplication;
+        resultText.text = score.ToString("")+"点";
+    }
+}
