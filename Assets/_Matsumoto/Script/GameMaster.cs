c904f721cb7092f5811e5dec96f30f91f116fb1a
DESKTOP-28F38VP\user <nkc.ryo.matsumoto@gmail.com>
2018-09-03 13:14:14 +0900

    ゲームのベースを作成

---
+++ Assets/_Matsumoto/Script/GameMaster.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+enum GameState {
+	Title,
+	Game,
+	Result,
+}
+
+public sealed class GameMaster : SingletonMonoBehaviour<GameMaster> {
+
+	GameState state;
+
+	public event UnityAction OnGameStart;
+	public event UnityAction OnGameOver;
+
+	protected override void Init() {
+		base.Init();
+
+		state = GameState.Title;
+		StartCoroutine(GameStartWait());
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+	public void GameStart() {
+		Debug.Log("GameStart");
+		state = GameState.Game;
+		OnGameStart();
+	}
+
+	public void GameOver() {
+		Debug.Log("GameOver");
+		state = GameState.Result;
+		OnGameOver();
+		StartCoroutine(GameOverWait());
+	}
+
+	public void GameReset() {
+		//今いるシーンを読み込み
+		OnGameStart = null;
+		OnGameOver = null;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		StartCoroutine(GameStartWait());
+	}
+
+
+	IEnumerator GameStartWait() {
+		yield return new WaitForSeconds(1f);
+		GameStart();
+	}
+
+	IEnumerator GameOverWait() {
+		yield return new WaitForSeconds(1f);
+		GameReset();
+	}
+}
---
+++ Assets/_Matsumoto/Script/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float movePower = 5f;
+	Rigidbody2D rig;
+
+	bool freeze = false;
+	public bool Freeze {
+		get {
+			return freeze;
+		}
+		set {
+			freeze = value;
+			rig.simulated = !value;
+		}
+	}
+
+	// Use this for initialization
+	void Awake () {
+		Debug.Log("Awake");
+		rig = GetComponent<Rigidbody2D>();
+		Freeze = true;
+		GameMaster.Instance.OnGameStart += () => Freeze = false; ;
+		GameMaster.Instance.OnGameOver += () => Freeze = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		MoveUpdate();
+	}
+
+	void MoveUpdate() {
+
+		rig.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * movePower, 0));
+
+	}
+}
