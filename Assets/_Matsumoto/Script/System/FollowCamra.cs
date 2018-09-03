90c68eb804636ea36e3bf7801e389405d469d5fe
DESKTOP-28F38VP\user <nkc.ryo.matsumoto@gmail.com>
2018-09-03 13:14:31 +0900

    プレイヤーについてくるカメラを作成

---
+++ Assets/_Matsumoto/Script/System/FollowCamra.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamra : MonoBehaviour {
+
+	public Transform target;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if(!target) return;
+
+		var pos = transform.position;
+		pos.y = target.position.y;
+		transform.position = pos;
+
+	}
+}
