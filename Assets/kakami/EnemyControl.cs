d0a51c4337be5ea5ca2973b4bf32d202e017dd14
Kakami Souma <nkc.souma.kakami@gmail.com>
2018-09-03 13:24:58 +0900

    敵の動き（仮）

---
+++ Assets/kakami/EnemyControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour {
+
+    [SerializeField, Header("敵の動き(左右)")]
+    bool moveChangeLeftRight;        //敵の左右の動きを変える
+
+    [SerializeField, Header("敵の行動")]
+    bool modeChange;                //敵の動きを変える
+
+    [SerializeField,Header("速度")]
+    float speed;                    //敵の速度
+
+
+
+    void EnemyMove()
+    {
+        switch (moveChangeLeftRight)
+        {
+            case true:
+                transform.position += new Vector3(speed,0,0);
+                break;
+
+            case false:
+                transform.position += new Vector3(speed*-1, 0, 0);
+                //if (transform.position.x)
+                //{
+
+                //}
+                break;
+        }
+
+        switch (modeChange)
+        {
+            case true:
+
+                break;
+
+            case false:
+
+                break;
+        }
+    }
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
