5e165316fe735798766fbd0a98eab95c99c6ac92
DESKTOP-28F38VP\user <nkc.ryo.matsumoto@gmail.com>
2018-09-03 14:54:49 +0900

    ギミックを追加

---
+++ Assets/_Matsumoto/Script/Gimmick/Bumper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+	public float power = 1;
+	public string playSEName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+
+		if(other.collider.tag != "Player") return;
+
+		var point = other.contacts[0].point;
+		var vec = ((Vector2)other.transform.position - point).normalized;
+		other.rigidbody.AddForce(vec * power);
+
+		AudioManager.PlaySE(playSEName);
+	}
+}
---
+++ Assets/_Matsumoto/Script/Gimmick/Cloud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+	public float attenuation = 0.1f;
+	public string playSEName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.tag != "Player") return;
+
+		other.GetComponent<Rigidbody2D>()
+			.velocity *= (1 - attenuation);
+
+		AudioManager.PlaySE(playSEName);
+	}
+}
